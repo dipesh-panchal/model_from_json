b12b57b98f75741e3cb496395a8ccaf478840dc7
dipesh-panchal <dipesh.p.panchal@gmail.com>
2026-02-03 09:41:11 +0530

    Initial CLI package scaffold

---
+++ bin/model_from_json.dart
@@ -0,0 +1,5 @@
+import 'package:model_from_json/model_from_json.dart' as model_from_json;
+
+void main(List<String> arguments) {
+  print('Hello world: ${model_from_json.calculate()}!');
+}
---
+++ test/model_from_json_test.dart
@@ -0,0 +1,8 @@
+import 'package:model_from_json/model_from_json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
